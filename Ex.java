32120322ce983dd13b8324b5dce771e9f766ebda
KD4-Atharva-83523 <apailwan55.com>
2024-04-01 18:52:14 +0530

    testing purpose upload file

---
+++ Ex.java
@@ -0,0 +1,23 @@
+class Test{
+	public void f1()
+	{
+		System.out.println("test::f1()");
+	}
+	
+
+}
+//{
+//	System.out.println("rahul");
+//}
+
+public class Ex 
+{
+	
+	public static void main(String [] args) 
+	{
+	System.out.println("hello pgm");
+	Test t1=new Test();
+	t1.f1();
+	}
+}
+
